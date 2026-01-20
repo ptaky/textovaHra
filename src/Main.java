cd3d93fce32852463e379190b5fa1de93c68fe0b
ptacek <ptondrej@gmail.com>
2026-01-20 09:08:56 +0100

    prvni hodina

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
+        ObjectMapper parser = new ObjectMapper();
+
+        try {
+            InputStream input = new FileInputStream("resource/NPC.json");
+            NPC npc = parser.readValue(input, NPC.class);
+            System.out.println(npc);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("FileNotFoundException");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception");
+        }
         System.out.println("Hello world!");
     }
 }
---
+++ src/NPC.java
@@ -0,0 +1,28 @@
+public class NPC {
+    private String name;
+    private int age;
+
+    // prazdny konstruktor je pro json nezbytny
+    public NPC() {}
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "NPC{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
