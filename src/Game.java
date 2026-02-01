31f71cd6f71e1c14d687c60955bb0fcc82ef9d2a
Ondřej Ptáček <ptondrej@gmail.com>
2026-02-01 21:20:40 +0100

    some details

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,20 @@
+import Command.Command;
+import java.util.HashMap;
+
 public class Game {
+    private HashMap<String, Command> commands;
 
     public void setup() {
         DataLoader dataLoader = new DataLoader();
         dataLoader.loadRoomsData();
+
+    }
+
+    public void CommandManager() {
+        commands = new HashMap<>();
+//        commands.put("end", new End_Command());
+//        commands.put("explore", new Explore_Command());
+//        commands.put("go", new Go_Command());
     }
 
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    Scanner scn = new Scanner(System.in);
+
+    public void commandExecute(String command, String input) {
+
+    }
+}
